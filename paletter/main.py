c3c38d6c9992a7c25540c61370f0a7fb31433319
Lain <vvz.dev@gmail.com>
2022-01-25 21:31:08 +0300

    cwd added

--- paletter/main.py
+++ paletter/main.py
@@ -2,26 +2,31 @@ import colors
 import image
 import cli
 import config
+import os
 
 
-def generate(image_path: str, out_name: str) -> None:
-    print('Getting color palette')
+def generate(image_path: str, out_path: str) -> None:
+    print('Getting color palette (this may take a while)')
     primary = colors.primary(image_path)
     palette = colors.palette(image_path)
-    brightest = max(palette, key=colors.rgb_to_luma)
+    brightest_color = max(palette, key=colors.rgb_to_luma)
+
     print('Generating image')
     blank = image.blank(config.SIZE, primary)
     out = image.add_color_blocks(blank, palette)
-    out = image.apply_borders(out, config.BORDER, brightest)
+    out = image.apply_borders(out, config.BORDER, brightest_color)
+
     print('Saving')
-    out.save(out_name)
-    print(f'Done! Saved as {out_name}')
+    out.save(out_path)
+
+    print(f'Done! Saved at {out_path}')
 
 
 def main():
-    image_path: str = cli.args.image
+    cwd = os.getcwd()
+    image_path = os.path.join(cwd, cli.args.image)
     image_extension = image_path.split('.')[-1]
-    generate(image_path, f'out.{image_extension}')
+    generate(image_path, os.path.join(cwd, f'out.{image_extension}'))
 
 
 if __name__ == "__main__":
